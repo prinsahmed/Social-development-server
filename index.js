b690fab9352d9c4a722fbd605f24626af63e4eaf
prinsahmed <s.a.prince.offiziell@gmail.com>
2025-11-11 21:34:28 +0600

    'create-event-upcoming-event-api-created'

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
+
+app.use(express.json())
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hi this is my server')
-})
 
 
-app.listen(3000)
+
+
+const uri = "mongodb+srv://myserver:1O7RI49eqHqgu6e7@cluster0.ufqnoyu.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const eventDB = client.db('eventDB');
+        const createEvent = eventDB.collection('createEvent')
+
+
+
+        app.post('/create-event', async(req, res) => {
+            const eventData = req.body;
+            const result = await createEvent.insertOne(eventData);
+            res.send(result)
+           
+        })
+
+
+        app.get('/events', async(req, res) => {
+
+            const result = await createEvent.find().toArray()
+            res.send(result)
+        })
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port)

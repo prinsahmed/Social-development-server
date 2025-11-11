01eadca0c045eee23ef050c9e563403d976e32bf
prinsahmed <s.a.prince.offiziell@gmail.com>
2025-11-11 19:45:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hi this is my server')
+})
+
+
+app.listen(3000)
